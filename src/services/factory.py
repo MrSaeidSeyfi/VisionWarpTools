3df3d219b2324d33cdf66fe887b0a60fe27b0759
saeid seyfi <mr.saeidseyfi@gmail.com>
2025-03-27 18:12:55 +0330

    Update factory.py

--- src/services/factory.py
+++ src/services/factory.py
@@ -1,7 +1,11 @@
 from src.services.image_processor import ImageProcessor
+from src.services.florence_detector import FlorenceDetector
 
 class ProcessorFactory:
-
     @staticmethod
     def create_processor():
         return ImageProcessor()
+    
+    @staticmethod
+    def create_florence_detector(model_path):
+        return FlorenceDetector(model_path)
